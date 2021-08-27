2996f8a4183d4b87fb9d09420c675f264f0a94f8
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-08-27 11:33:55 -0400

    got help

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,17 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+// import user from "../data/user";
+// console.log(user);
+
+function User(){
+  return (
+    <div>
+      
+    </div>
+  )
+
+}
 
 function App() {
   return (
