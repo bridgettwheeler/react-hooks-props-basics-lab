c784aaf7321c85892a0c2e4edc4431d299046fba
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-08-27 12:17:14 -0400

    got help

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  if (!props.bio) {
+    return null
+  } else {}
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p> {props.bio} </p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <a href={props.linkedin}> Linkedin </a> 
+      <a href={props.github}> Github </a>
+      
+
     </div>
   );
 }
